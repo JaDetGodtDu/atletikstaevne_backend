e239291e0b326e5846bed05ae1b67a9e05566da6
JaDetGodtDu <123992392+JaDetGodtDu@users.noreply.github.com>
2024-06-20 10:42:11 +0200

    Discipline and result value

--- src/main/java/com/example/atletikstaevne_backend/entity/Discipline.java
+++ src/main/java/com/example/atletikstaevne_backend/entity/Discipline.java
@@ -1,4 +1,24 @@
 package com.example.atletikstaevne_backend.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name= "discipline")
 public class Discipline {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "result_type")
+    private String resultType;
 }
--- src/main/java/com/example/atletikstaevne_backend/entity/Result.java
+++ src/main/java/com/example/atletikstaevne_backend/entity/Result.java
@@ -1,4 +1,38 @@
 package com.example.atletikstaevne_backend.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name= "result")
 public class Result {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name="result_value")
+    private String resultValue;
+
+    @ManyToOne
+    @JoinColumn(name = "contestant_id", referencedColumnName = "id")
+    private Contestant contestant;
+
+    @ManyToOne
+    @JoinColumn(name = "discipline_id", referencedColumnName = "id")
+    private Discipline discipline;
+
+    public String getResultType(){
+        return discipline.getResultType();
+    }
 }
